f46e063420e33880dbeb4705fd9b9a45a33161d3
Elmar Quluzade <elmarguluzade1@gmail.com>
2022-11-15 11:45:10 +0400

    update-10

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ class Music{
 };
 
 const musicList = [
-    new Music("Demet Akalin","Ben Fero","1.jpg","1.mp3"),
-    new Music("1,2,3","Ben Fero","3.jpg","3.mp3"),
+    new Music("Sia","Unstoppable","1.jpg","1.mp3"),
     new Music("Dua Lipa","One Kiss","2.jpg","2.mp3")
 ];
 
